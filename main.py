93144a2679cfa4127c989b4f6d0bde0a0d265b01
Shayem1 <shayemiam@gmail.com>
2025-02-21 16:27:32 +1100

    fb

--- main.py
+++ main.py
@@ -7,18 +7,43 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from selenium import webdriver
 
+#button timeouts
+def active():
+    global cont_button
+    cont_button.configure(state=NORMAL)
+
+
+def phase_3():
+    global driver, cont_button, label
+    pass
+
 
 def phase_2():
 
-    #move to 
+    #move to
+    global status, driver, label, cont_button
     status = 2
 
+    #opens the next webpage
+    driver.get("https://ibma.app.axcelerate.com/management/management2/ContactsSearch.cfm")
 
+    #removes old info
+    label.destroy()
+    cont_button.destroy()
+
+    #next instructions
+    label.configure(master=GUI, text="Please select optional id\nand sort by optional id", font=("arial",20))
+    label.pack(pady=15)
+
+    cont_button.configure(text="Continue", font=("arial",30), corner_radius=30, state=DISABLED,  command= lambda: phase_3())
+    cont_button.pack(pady=15)
+
+    #ensures user does the action
+    GUI.after(5000, active)
 
 def phase_1():
-    
     #move to first stage
-    global status, label2, cont_button1
+    global status, label, cont_button, driver
     status = 1
 
     #opens chrome page
@@ -27,14 +52,16 @@ def phase_1():
     
     #removes objects from previous stage
     cont_button.destroy()
-    label1.destroy()
+    label.destroy()
 
     #adds instructions for the next stage
-    label2 = customtkinter.CTkLabel(master=GUI, text="Please Login to any account\n with access to merge mailout groups\n\nPress continue AFTER you login", font=("arial",20))
-    label2.pack(pady=15)
+    label.configure(text="Please Login to any account\n with access to merge mailout groups\n\nPress continue AFTER you login", font=("arial",20))
+    label.pack(pady=15)
 
-    cont_button1 = customtkinter.CTkButton(master = GUI, text="Continue", font=("arial",30), corner_radius=30, command= lambda: phase_2())
-    cont_button1.pack(pady=15)
+    cont_button.configure(text="Continue", font=("arial",30), corner_radius=30, state=DISABLED,  command= lambda: phase_2())
+    cont_button.pack(pady=15)
+
+    GUI.after(20000, active)
 
 #helps code remember what phase it is on
 status = 0
@@ -47,9 +74,11 @@ GUI.minsize(width=450, height=200)
 GUI.maxsize(width=450, height=200)
 customtkinter.set_appearance_mode("dark")
 
+global label, cont_button
+
 #setting up info text for phase 0
-label1 = customtkinter.CTkLabel(master=GUI, text="Hello, Tasker will guide you on how\nto automatically make merge out groups\nin seconds from the attendance sheet", font=("arial",20))
-label1.pack(pady=15)
+label = customtkinter.CTkLabel(master=GUI, text="Hello, Tasker will guide you on how\nto automatically make merge out groups\nin seconds from the attendance sheet", font=("arial",20))
+label.pack(pady=15)
 
 cont_button = customtkinter.CTkButton(master = GUI, text="Continue", font=("arial",30), corner_radius=30, command= lambda: phase_1())
 cont_button.pack(pady=30)
